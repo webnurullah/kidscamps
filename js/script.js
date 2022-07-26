1f467babd235a8470fe4afdc9ea5e977b968cc3b
Farabi islam Nurullah <farabi48islam@gmail.com>
2022-07-26 14:09:39 +0600

    max upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  
+    $(".message-hover").hover(function(){
+      $(".hover-address-box").css("opacity", "1");
+ 
+    }, function(){
+        $(".hover-address-box").css("opacity", "0");
+      
+    });
+   
+  });
+
+
