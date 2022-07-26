1a552e63bb95c723d09dc2f1cd26cbf8bd7ff1d5
Farabi islam Nurullah <farabi48islam@gmail.com>
2022-07-26 18:57:03 +0600

    final update

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
   
     $(".message-hover").hover(function(){
-      $(".hover-address-box").css("opacity", "1");
+      $(".hover-address-box").css({"opacity": "1", "z-index": "20"});
  
     }, function(){
-        $(".hover-address-box").css("opacity", "0");
+        $(".hover-address-box").css({"opacity": "0", "z-index": "-1"});
       
     });
    
